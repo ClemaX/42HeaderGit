acac1a8254f6852efa748a917408df093f1329a7
Clément Hamada <chamada@student.le-101.fr>
2020-12-13 14:50:14 +0100

    Implement insert header

--- 42header.sh
+++ 42header.sh
@@ -8,7 +8,6 @@ HEIGHT=11
 WIDTH=80
 MARGIN=5
 
-
 ASCII_ART=(
 "        :::      ::::::::"
 "      :+:      :+:    :+:"
@@ -24,25 +23,13 @@ START="/*"
 END="*/"
 FILL="*"
 
+HEADER_REGEX="^/\*.*\*/$"
+
 FILL_LENGTH=$(($WIDTH / ${#FILL} - ${#START} - ${#END} - 2))
 PADDING_LEFT=$(($MARGIN - ${#START}))
 PADDING_RIGHT=$(($MARGIN - ${#END}))
 
 CONTENT_WIDTH=$(($WIDTH - $ASCII_ART_WIDTH - 2 * $MARGIN))
-
-HEADER="\
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   bg.c                                               :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: pablo <pablo@student.42lyon.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/18 23:11:42 by pablo             #+#    #+#             */
-/*   Updated: 2020/12/12 21:59:55 by pablo            ###   ########lyon.fr   */
-/*                                                                            */
-/* ************************************************************************** */\
-"
 
 git_time_created()
 {
@@ -63,11 +50,11 @@ header_fill()
 
 header_content()
 {
-	printf "$START"
+	printf '%s' "$START"
 	printf ' %.0s' $(eval "echo {1.."$(($PADDING_LEFT))"}")
 	printf '%-*s' "$CONTENT_WIDTH" "$1" "$ASCII_ART_WIDTH" "$2"
 	printf ' %.0s' $(eval "echo {1.."$(($PADDING_RIGHT))"}")
-	printf "$END\n"
+	printf "%s\n" "$END"
 }
 
 header()
@@ -85,16 +72,47 @@ header()
 		header_content "" "${ASCII_ART[2]}"
 		header_content "By: $USER <$MAIL>" "${ASCII_ART[3]}"
 		header_content "" "${ASCII_ART[4]}"
-		header_content "Created: $CREATED_AT" "${ASCII_ART[5]}"
-		header_content "Updated: $UPDATED_AT" "${ASCII_ART[6]}"
+		header_content "Created: $CREATED_AT by: $USER" "${ASCII_ART[5]}"
+		header_content "Updated: $UPDATED_AT by: $USER" "${ASCII_ART[6]}"
 
 		header_fill " " "$FILL_LENGTH"
 		header_fill "$FILL" "$FILL_LENGTH"
 	fi
 }
 
-header srcs/main/main.c
+has_header()
+{
+	head -11 "$1" | grep -vq "$HEADER_REGEX" && return 1 || return 0
+}
 
-#for src in **/*.c; do
-#	echo $src: $(git_time_created $src) $(git_time_updated $src)
-#done
+insert_header()
+{
+	printf '%s\n\n' "$(header "$1")"\
+		| cat - "$1" > .header_tmp\
+		&& mv .header_tmp "$1"
+}
+
+update_header()
+{
+	if has_header "$1" ; then
+		: #echo "TODO: Update header!"
+	else
+		insert_header "$1"
+	fi
+}
+
+# Change to the sources directory
+cd srcs/
+
+# Update headers in .c files
+for src in */**.c; do
+	update_header "$src"
+done
+
+# Change to the includes directory
+cd ../includes/
+
+# Update headers in .h files
+for head in */**.h; do
+	update_header "$head"
+done
